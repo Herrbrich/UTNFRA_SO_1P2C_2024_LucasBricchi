db5a1c8cd4949e61479ac0b496c9aea23cd7bf25
Herrbrich <lucasbricchi12@gmail.com>
2024-10-13 15:16:41 +0000

    ADD: Punto A terminado

---
+++ RTA_SCRIPT_Examen_20241012/Punto_A.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p repogit/UTNFRA_SO_1P2C_2024_LucasBricchi/RTA_ARCHIVOS_Examen_20241012/Examenes-UTN/alumno_{1..3}/parcial_{1..3} repogit/UTNFRA_SO_1P2C_2024_LucasBricchi/RTA_ARCHIVOS_Examen_20241012/Examenes-UTN/profesores
+
